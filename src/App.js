d61a4976a4f5d25cf7ea3c05baa43414362f612f
Thorsten Kober <info@flashdesignory.com>
2020-03-18 22:03:17 -0700

    adjust colors of buttons

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import { ButtonTypes } from './constants/button';
 function App() {
   return (
     <div className="App">
-      <Button label={"click me"} type={ButtonTypes.PRIMARY} onClick={() => console.log('click')}/>
-      <Button label={"click me"} type={ButtonTypes.SECONDARY} onClick={() => console.log('click')}/>
-      <Button label={"click me"} type={ButtonTypes.TERTIARY} onClick={() => console.log('click')}/>
+      <div>
+        <div>Primary Button:</div>
+        <Button label={"click me"} type={ButtonTypes.PRIMARY} onClick={() => console.log('click')}/>
+      </div>
+      <div>
+        <div>Secondary Button:</div>
+        <Button label={"click me"} type={ButtonTypes.SECONDARY} onClick={() => console.log('click')}/>
+      </div>
+      <div>
+        <div>Tertiary Button:</div>
+        <Button label={"click me"} type={ButtonTypes.TERTIARY} onClick={() => console.log('click')}/>
+      </div>
     </div>
   );
 }
